605d6166cd63b677dc0059c660d491554daa8061
Wilfred Hughes <me@wilfred.me.uk>
2013-08-08 14:49:50 +0100

    ahem, fixing copy-paste error

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='django_function_cache',
+setup(name='django_test_mixins',
       description="Additional assertions and test conveniences for testing django sites.",
       version='1.0',
       url="https://github.com/Wilfred/django-test-mixins",
