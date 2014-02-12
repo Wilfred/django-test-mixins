649313265ef06d909a0dafdf07fa4b56511f7e62
Wilfred Hughes <me@wilfred.me.uk>
2014-02-12 19:13:55 +0000

    Adding unit tests for FormValidationTestCase.

--- tests.py
+++ tests.py
@@ -11,9 +11,9 @@ settings.configure(DATABASES={
     }
 })
 
-from django_test_mixins import HttpCodeTestCase
+from django_test_mixins import HttpCodeTestCase, FormValidationTestCase
 
-class AssertionTest(HttpCodeTestCase):
+class HttpAssertionsTest(HttpCodeTestCase):
     def test_http_ok(self):
         self.assertHttpOK(Mock(status_code=200))
 
@@ -38,6 +38,29 @@ class AssertionTest(HttpCodeTestCase):
 
         with self.assertRaises(AssertionError):
             self.assertHttpRedirect(Mock(status_code=200))
+
+
+class FormAssertionsTest(FormValidationTestCase):
+    def test_form_invalid(self):
+        """If we have a request containing a form with .is_valid returning
+        False, then the form was indeed invalid.
+
+        """
+        mock_form = Mock()
+        mock_form.is_valid.return_value = False
+        
+        mock_request = Mock(context={'form': mock_form}, status_code=200)
+        self.assertFormInvalid(mock_request)
+
+    def test_form_invalid_fails_on_redirect(self):
+        """If the form was valid, the most common scenario is that we were
+        redirected and there's no form present.
+
+        """
+        mock_request = Mock(status_code=302, context={})
+
+        with self.assertRaises(AssertionError):
+            self.assertFormInvalid(mock_request)
 
 
 if __name__ == '__main__':
